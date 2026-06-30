a8f0692ae67e5fd47dad71321995c30914072c3c
AJkz17 <magicmoon1344@gmail.com>
2026-07-01 04:52:30 +0800

    updated Main funvtions

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,24 @@
 import Image from "next/image";
 import Landing from "./Components/Landing";
+import { query } from './lib/db';
 
-export default function LandingPage() {
+export default async function LandingPage() {
+
+  let dbTime = "Connecting...";
+  let connectionStatus = "🔴 Connection Failed";
+
+  try {
+    // Run the native postgres test query
+    const result = await query('SELECT NOW()');
+    if (result.rows.length > 0) {
+      dbTime = result.rows[0].now.toString();
+      connectionStatus = "🟢 Successfully Connected to TravelProject!";
+    }
+  } catch (err) {
+    console.error(err);
+  }
+
+
   return (
     <Landing />
   );
