263e37af48daf78a4011683abfde4ea96b140dc3
AJkz17 <magicmoon1344@gmail.com>
2026-07-04 13:48:22 +0800

    import & export new images

--- app/assets/asset.js
+++ app/assets/asset.js
@@ -1,3 +1,7 @@
 import MalaysiaImage from './Malaysia.jpg';
+import MapPicture from './Map-picture.jpg';
+import TwinTower from './TT.jpg';
 
 export default MalaysiaImage;
+export { MapPicture};
+export const twinTowerUrl = TwinTower.src;
